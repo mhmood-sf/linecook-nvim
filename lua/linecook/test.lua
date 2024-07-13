b70bf1020401ceb6bf0e714b312ad7c21be0a778
mhmood-sf <28828855+mhmood-sf@users.noreply.github.com>
2024-07-13 13:41:31 -0400

    Add test file

---
+++ lua/linecook/test.lua
@@ -0,0 +1,39 @@
+local linecook = require("linecook")
+local lc_components = require("linecook.components")
+local lc_containers = require("linecook.containers")
+
+local myModeContainer = linecook.mk_container {
+    name = "myModeContainer",
+    style = { fg = "black", bg = "white" },
+    right = {
+        sep = "",
+        pad = 1,
+        style = { fg = "white" }
+    },
+    left = {
+        sep = "",
+        pad = 1
+    }
+}
+
+local myFtContainer = lc_containers.plain("myFtContainer", {
+    bg = "white", fg = "black"
+})
+
+local function myModeComponent()
+    return myModeContainer(string.upper(vim.fn.mode()))
+end
+
+local function myFtComponent(stl)
+    return myFtContainer(vim.api.nvim_buf_get_option(stl.bufnr, "filetype"))
+end
+
+local render = linecook.mk_renderer {
+    myModeComponent,
+    lc_components.divider,
+    myFtComponent,
+}
+
+vim.o.stl = "%!v:lua.require'linecook.test'.render()"
+
+return { render = render }
